0714746bef17c8b8d32de14af54de46964456235
paolomococci <37364757+paolomococci@users.noreply.github.com>
2019-05-21 04:28:18 +0200

    Create WordDao.java

---
+++ app/src/main/java/local/example/word/dao/WordDao.java
@@ -0,0 +1,53 @@
+/**
+ *
+ * Copyright 2019 paolo mococci
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * 	   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package local.example.word.dao;
+
+import androidx.lifecycle.LiveData;
+import androidx.room.Dao;
+import androidx.room.Delete;
+import androidx.room.Insert;
+import androidx.room.OnConflictStrategy;
+import androidx.room.Query;
+import androidx.room.Update;
+
+import java.util.List;
+
+import local.example.word.entity.Word;
+
+@Dao
+public interface WordDao {
+
+    @Insert(onConflict = OnConflictStrategy.IGNORE)
+    void insert(Word word);
+
+    @Query("DELETE FROM word_table")
+    void deleteAll();
+
+    @Query("SELECT * FROM word_table LIMIT 1")
+    Word[] getAnyWord();
+
+    @Query("SELECT * FROM word_table ORDER BY word ASC")
+    LiveData<List<Word>> getAllWords();
+
+    @Update
+    void update(Word... word);
+
+    @Delete
+    void deleteWord(Word word);
+}
