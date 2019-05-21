ab27c392fba3aa5c9d205e3b7dccdef54fa63809
paolomococci <37364757+paolomococci@users.noreply.github.com>
2019-05-21 04:26:53 +0200

    Create Word.java

---
+++ app/src/main/java/local/example/word/entity/Word.java
@@ -0,0 +1,58 @@
+/**
+ *
+ * Copyright 2019 paolo mococci
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * 	   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package local.example.word.entity;
+
+import androidx.annotation.NonNull;
+import androidx.room.ColumnInfo;
+import androidx.room.Entity;
+import androidx.room.Ignore;
+import androidx.room.PrimaryKey;
+
+@Entity(tableName = "word_table")
+public class Word {
+
+    @PrimaryKey(autoGenerate = true)
+    private int id;
+
+    @NonNull
+    @ColumnInfo(name = "word")
+    private String word;
+
+    public Word(@NonNull String word) {
+        this.word = word;
+    }
+
+    @Ignore
+    public Word(int id, @NonNull String word) {
+        this.id = id;
+        this.word = word;
+    }
+
+    public String getWord() {
+        return this.word;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+}
